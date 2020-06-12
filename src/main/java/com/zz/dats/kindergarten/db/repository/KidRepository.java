aade1740680a99345364a3e8fe7e8591038e500a
Maris Locmelis <m.locmelis@dynatech.lv>
2020-06-12 19:35:31 +0300

    * fixed filter out records.

--- src/main/java/com/zz/dats/kindergarten/db/repository/KidRepository.java
+++ src/main/java/com/zz/dats/kindergarten/db/repository/KidRepository.java
@@ -9,6 +9,10 @@ import java.util.Collection;
 
 @Repository
 public interface KidRepository extends JpaRepository<KidEntity, Integer> {
-    @Query("FROM KidEntity AS k LEFT JOIN k.kindergartenKidsById AS kk LEFT JOIN k.queuesById as q WHERE (q.id IS NULL OR q.kindergartenByKindergartenId.id <> ?1) AND kk.id IS NULL")    //This is using a named query method
+    @Query(
+        value="SELECT DISTINCT k.* FROM kid as k LEFT JOIN queue q on k.id = q.kid_id LEFT JOIN kindergarten_kids kk on k.id = kk.kid_id " +
+            "WHERE q.kid_id NOT IN (SELECT kid_id FROM queue WHERE kindergarten_id = ?1) AND kk.id IS NULL", nativeQuery=true
+//        "FROM KidEntity AS k LEFT JOIN k.kindergartenKidsById AS kk LEFT JOIN k.queuesById as q WHERE q.kidByKidId.id NOT IN (SELECT kidByKidId.id FROM QueueEntity WHERE q.kindergartenByKindergartenId.id = ?1 ) AND kk.id IS NULL"
+    )
     Collection<KidEntity> getKidsAvailableFor(Integer kindergartenId);
 }
